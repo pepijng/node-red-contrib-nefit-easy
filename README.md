7663f05268597b5b5b70f1ce8fc173e40a0cad4e
Pepijn <RAIL0024381@RAIL-LTP00848.prod.bam.nl>
2016-01-05 23:09:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Nefit Easy™ Node-RED Node
+
+This Node-RED Node is based on the <a href="https://github.com/robertklep/nefit-easy-client">Nefit Easy™ client library build by Robert Klep</a>
---
+++ nefit-easy/nefit-easy.js
@@ -0,0 +1,136 @@
+
+module.exports = function(RED) {
+    "use strict";
+
+    const Promise         = require('bluebird');
+    const NefitEasyClient = require('./nefit-easy-client/index.js');
+    
+    function NefitEasyConfigNode(n) {
+        RED.nodes.createNode(this,n);
+        this.serialNumber = n.serialNumber; 
+        this.accessKey = n.accessKey;
+        this.password = n.password;
+    }
+    RED.nodes.registerType("nefit-easy-config",NefitEasyConfigNode);
+    
+    function NefitEasyNode(n) {
+        // Create a RED node
+        RED.nodes.createNode(this,n);
+        
+        this.topic = n.topic;
+        this.command = n.command;
+        this.easy = n.easy;
+        
+        var node = this;
+        var msg = {};
+        this.easyconfig = RED.nodes.getNode(this.easy);
+         if (this.easyconfig) {
+            node.serialNumber = this.easyconfig.serialNumber;
+            node.accessKey = this.easyconfig.accessKey;
+            node.password = this.easyconfig.password;
+         } else { node.error("GEEN CONFIG"); }
+        
+        this.on('input', function (msg) {
+
+        const client = NefitEasyClient({
+                        serialNumber : node.serialNumber,
+                        accessKey    : node.accessKey,
+                        password     : node.password,
+                        });            
+            
+            
+        if (node.command == 'status') {
+            client.connect().then( () => {
+                return Promise.all([ client.status() ]);
+                }).spread((status) => {
+                    msg.topic = this.topic;
+                    msg.payload = {};
+                    msg.payload = status;
+                    this.send(msg);
+                }).catch((e) => {
+                    node.error(e)
+                }).finally(() => {
+                    client.end();
+                });
+
+        } else if (node.command == 'pressure') {
+            client.connect().then( () => {
+                return Promise.all([ client.pressure() ]);
+                }).spread((pressure) => {
+                    msg.topic = this.topic;
+                    msg.payload = {};
+                    msg.payload = pressure;
+                    this.send(msg);
+                }).catch((e) => {
+                    node.error(e)
+                }).finally(() => {
+                    client.end();
+                });
+        } else if (node.command == 'location') {
+            client.connect().then( () => {
+                return Promise.all([ client.location() ]);
+                }).spread((location) => {
+                    msg.topic = this.topic;
+                    msg.payload = {};
+                    msg.payload = location;
+                    this.send(msg);
+                }).catch((e) => {
+                    node.error(e)
+                }).finally(() => {
+                    client.end();
+                });
+        } else if (node.command == 'program') {
+            client.connect().then( () => {
+                return Promise.all([ client.program() ]);
+                }).spread((program) => {
+                    msg.topic = this.topic;
+                    msg.payload = {};
+                    msg.payload = program;
+                    this.send(msg);
+                }).catch((e) => {
+                    node.error(e)
+                }).finally(() => {
+                    client.end();
+                });
+        } else if (node.command == 'display-code') {
+            client.connect().then( () => {
+                return Promise.all([ client.displayCode() ]);
+                }).spread((displayCode) => {
+                    msg.topic = this.topic;
+                    msg.payload = {};
+                    msg.payload = displayCode;
+                    this.send(msg);
+                }).catch((e) => {
+                    node.error(e)
+                }).finally(() => {
+                    client.end();
+                });
+        } else if (node.command == 'set-temperature') {
+                node.warn('Set temp: '+msg.payload);
+            client.connect().then( () => {
+                return Promise.all([ client.setTemperature(Number(msg.payload)) ]);
+                }).spread((setTemperature) => {
+                    msg.topic = this.topic;
+                    msg.payload = {};
+                    msg.payload = setTemperature;
+                    this.send(msg);
+                }).catch((e) => {
+                    node.error(e)
+                }).finally(() => {
+                    client.end();
+                })            
+                
+                
+        }
+            
+        });
+
+        this.on("close", function() {
+
+        });
+    }
+    RED.nodes.registerType("nefit-easy",NefitEasyNode);
+    
+
+
+}
